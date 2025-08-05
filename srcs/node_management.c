3cb87101f221c158ca609e93dfcd43cbdf6ec74a
ramune256 <shungo180620@outlook.jp>
2025-08-05 20:07:57 +0900

    changed not to use Libft funcs

--- srcs/node_management.c
+++ srcs/node_management.c
@@ -6,27 +6,34 @@
 /*   By: shunwata <shunwata@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/29 14:16:42 by shunwata          #+#    #+#             */
-/*   Updated: 2025/08/03 17:05:05 by shunwata         ###   ########.fr       */
+/*   Updated: 2025/08/05 20:07:12 by shunwata         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+#include "push_swap.h"
+
 void	add_node(t_stack *stack, int value)
 {
-	t_node		*new_node;
+	t_node	*new_node;
 
-	new_node = ft_lstnew(value);
+	new_node = (t_node *)malloc(sizeof(t_node));
 	if (!new_node)
-		return (write(2, "Error\n", 6));
-	ft_lstadd_front(&(stack->top), new_node);
+	{
+		write(2, "Error\n", 6);
+		return;
+	}
+	new_node->value = value;
+	new_node->next = stack->top;
+	stack->top = new_node;
 	stack->size++;
 }
 
 int	del_node(t_stack *stack)
 {
-	t_node		*top_node;
-	int			value;
+	t_node	*top_node;
+	int		value;
 
 	if (stack->size == 0)
 		return (0);
@@ -40,8 +47,8 @@ int	del_node(t_stack *stack)
 
 void	swap(t_stack *stack)
 {
-	int			first;
-	int			second;
+	int	first;
+	int	second;
 
 	if (stack->size < 2)
 		return;
@@ -53,27 +60,34 @@ void	swap(t_stack *stack)
 
 void	rotate(t_stack *stack)
 {
-	t_node		*old_top;
+	t_node	*old_top;
+	t_node	*tail;
 
 	if (stack->size < 2)
 		return;
 	old_top = stack->top;
 	stack->top = stack->top->next;
 	old_top->next = NULL;
-	ft_lstadd_back(&(stack->top), old_top);
+	tail = stack->top;
+	while (tail && tail->next)
+		tail = tail->next;
+	tail->next = old_top;
 }
 
 void	reverse_rotate(t_stack *stack)
 {
-	t_node		*tail;
-	t_node		*prev;
+	t_node	*tail;
+	t_node	*prev;
 
 	if (stack->size < 2)
 		return;
-	tail = ft_lstlast(stack->top);
-	prev = stack->top;
-	while (prev->next != tail)
-		prev = prev->next;
+	tail = stack->top;
+	while (tail->next)
+	{
+		prev = tail;
+		tail = tail->next;
+	}
 	prev->next = NULL;
-	ft_lstadd_front(&(stack->top), tail);
+	tail->next = stack->top;
+	stack->top = tail;
 }
